12cce6c8ef34d17d3feb880e9f5a773807172d14
Scott Serage <xttocsurge51@gmail.com>
2017-09-09 15:51:09 -0600

    Completed FilterString functionality

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,9 +1,43 @@
 import React, {Component} from 'react'
 
 class FilterString extends Component {
+  constructor(){
+    super();
+    this.state = {
+      unFilteredArray: [
+        "Mike",
+        "May",
+        "Krissy",
+        "Lou",
+        "Sal",
+        "Mary",
+        "Mikel",
+        "Louis",
+        "Sally",
+        "Kristian"
+      ],
+      userInput: "",
+      filteredArray: []
+    }
+  }
+
+  updateUserInput(newValue){this.setState({userInput: newValue})}
+
+  solve(){
+    this.setState({
+      filteredArray: this.state.unFilteredArray.filter(e => e.substring(0,this.state.userInput.length) === this.state.userInput)
+    })
+  }
+
   render(){
     return(
-      <p>FilterString Component</p>
+      <div className="puzzleBox filterStringPB">
+        <h4>Filter String</h4>
+        <span className="puzzleText">Names: {JSON.stringify(this.state.unFilteredArray)}</span>
+        <input className="inputLine" onChange={ (e)=> this.updateUserInput(e.target.value) }/>
+        <button className="confirmationButton" onClick={ ()=> this.solve() }></button>
+        <span className="resultsBox filterStringRB">Filtered Names: {JSON.stringify(this.state.filteredArray)}</span>
+      </div>
     )
   }
 }
